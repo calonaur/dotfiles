f455f08d7857b99112304e28cd440e0420c3e123
Harrison Taylor <harritaylor@protonmail.com>
2020-10-31 10:21:52 +0000

    add profile and input

--- setup.sh
+++ setup.sh
@@ -11,5 +11,9 @@ ln -sf "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
 ln -sf "$PWD/gitignore" "$HOME/.gitignore"
 ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
 
-# login
+# login & input
 ln -sf "$PWD/hushlogin" "$HOME/.hushlogin"
+ln -sf "$PWD/inputrc" "$HOME/.inputrc"
+
+# bash
+ln -sf "$PWD/profile" "$HOME/.profile"
