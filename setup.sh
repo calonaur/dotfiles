be88cfa413742f49dc641ace1acfc60956f0e8f6
Harrison Taylor <harritaylor@protonmail.com>
2020-10-31 10:15:49 +0000

    shut up when logging in

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ ln -sf "$PWD/init.vim" "$HOME/.config/nvim/init.vim"
 # git
 ln -sf "$PWD/gitignore" "$HOME/.gitignore"
 ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
+
+# login
+ln -sf "$PWD/hushlogin" "$HOME/.hushlogin"
